2233640b770bec507249c0b9d8a117884a45d135
AlienTheBetrayer <gpichkurov@gmail.com>
2019-11-23 23:25:47 +0200

    calc

---
+++ commands/calc.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+function calc(equation) {
+return new Function('return ' + equation)();
+}
+
+module.exports.run = async(client, message, args) => {
+    if(args[0] == undefined) return;
+    const eq = args[0];
+    const embed = new Discord.RichEmbed()
+    .setTitle("Calculation")
+    .setTimestamp()  
+    .setColor(0x3fb5a1)
+    .setAuthor(message.author.username + "#" + message.author.discriminator, message.author.displayAvatarURL)
+    .addField("Equation", eq)
+    .addField("Result", calc(eq))
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "calc",
+    aliases: ["calculate"]
+}
