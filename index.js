9f006fd843fad37dfd61ce1ee85f0ff7916686bb
AlienTheBetrayer <54509121+AlienTheBetrayer@users.noreply.github.com>
2019-12-31 12:22:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,17 +38,19 @@ function sleep(ms) {
   }
 
 client.on("ready", () => { // setup
-    client.user.setActivity("Stable | " + prefix + "help", {type: "STREAMING", url: "https://www.twitch.tv/alienbetrayer"});
+    client.user.setActivity("🎄Stable | " + prefix + "help🎄", {type: "STREAMING", url: "https://www.twitch.tv/alienbetrayer"});
     console.log("Client is online.");
 });
 
 client.on("message", message => {    // message event
    if(message.author.bot) return;
+   if(message.content.startsWith("$")) {
    const parts = message.content.split(" ");// message parsing
    const command = parts[0];
    const args = parts.slice(1);
    let commandFile = client.commands.get(command.slice(prefix.length)) || client.commands.get(client.aliases.get(command.slice(prefix.length)))
    if(commandFile) commandFile.run(client, message, args)
+   }
 });
 
 client.on("guildMemberAdd", member => {
